51d30d3fc6e4ec19cc6854768b31b6dd627a560c
kvqn <guneetaggarwal@gmail.com>
2023-03-30 15:22:42 +0530

    layout for django/web

--- echiofy/echiofy/urls.py
+++ echiofy/echiofy/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from web import views as web_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', web_views.homepage, name='homepage'),
+    path('find_influencers/', web_views.find_influencers, name='find_influencers')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- echiofy/web/views.py
+++ echiofy/web/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+TEMPLATE_HOMEPAGE = "homepage.html"
+TEMPLATE_FIND_INFLUENCERS = "find_influencers.html"
+
+
+def homepage(request):
+    context = {}
+    return render(request, TEMPLATE_HOMEPAGE, context)
+
+def find_influencers(request):
+    context = {}
+    return render(request, TEMPLATE_FIND_INFLUENCERS, context)
