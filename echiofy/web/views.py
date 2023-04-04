367611829ce78819c6df7e390bc4db94ee9e891a
kvqn <guneetaggarwal@gmail.com>
2023-04-04 14:14:34 +0530

    url bindings

--- echiofy/web/views.py
+++ echiofy/web/views.py
@@ -2,18 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 TEMPLATE_HOMEPAGE = "homepage.html"
-TEMPLATE_FIND_INFLUENCERS = "find_influencers.html"
+TEMPLATE_LOGIN = "login.html"
+TEMPLATE_REGISTER = "register.html"
 TEMPLATE_CREATE_CAMPAIGN = "create_campaign.html"
+TEMPLATE_CAMPAIGNS = "campaigns.html"
+TEMPLATE_FIND_INFLUENCERS = "find_influencers.html"
 
 
 def homepage(request):
     context = {}
     return render(request, TEMPLATE_HOMEPAGE, context)
 
-def find_influencers(request):
+def login(request):
     context = {}
-    return render(request, TEMPLATE_FIND_INFLUENCERS, context)
+    return render(request, TEMPLATE_LOGIN, context)
+
+def signup(request):
+    context = {}
+    return render(request, TEMPLATE_REGISTER, context)
 
 def create_campaign(request):
     context = {}
     return render(request, TEMPLATE_CREATE_CAMPAIGN, context)
+
+def campaigns(request):
+    context = {}
+    return render(request, TEMPLATE_CAMPAIGNS, context)
+
+def find_influencers(request):
+    context = {}
+    return render(request, TEMPLATE_FIND_INFLUENCERS, context)
+
