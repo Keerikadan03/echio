0e324df20f25b7508cee24ac7f989144d38a4643
kvqn <guneetaggarwal@gmail.com>
2023-04-18 19:50:53 +0530

    fix : campaign form optional fields

--- echiofy/campaign/forms.py
+++ echiofy/campaign/forms.py
@@ -8,8 +8,8 @@ class CampaignCreationForm(forms.Form):
     description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Campaign Description'}), required=True)
     primary_objective = forms.CharField(widget=forms.Select(choices=[(tag, tag.value) for tag in PrimaryObjectiveEnum], attrs={'class': 'form-select'}), required=True)
     brand = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Brand'}), required=True)
-    payment_type = forms.CharField(widget=forms.Select(choices=[(tag, tag.value) for tag in PaymentTypeEnum], attrs={'class': 'form-select'}), required=True)
-    budget = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    payment_delay_days = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    tentative_payout = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    payment_type = forms.CharField(widget=forms.Select(choices=[(tag, tag.value) for tag in PaymentTypeEnum], attrs={'class': 'form-select'}), required=False)
+    budget = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    payment_delay_days = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
+    tentative_payout = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}), required=False)
 
