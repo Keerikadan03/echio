3a08824896a9e6313567b6b2590557807308097d
kvqn <guneetaggarwal@gmail.com>
2023-04-18 18:52:24 +0530

    used backend genenated input field for identical form

--- echiofy/user/forms.py
+++ echiofy/user/forms.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import UserProfile
+from django import forms
 
 
 # Create your models here.
@@ -9,6 +10,16 @@ class UserRegisterForm(UserCreationForm):
         model = UserProfile
         fields = ['username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'is_influencer']
 
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+    is_influencer = forms.BooleanField(widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
+
+
+
 class UserLoginForm(AuthenticationForm):
-    def confirm_login_allowed(self, user):
-        pass
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
