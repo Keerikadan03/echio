174ce95267179a1fefbd7d20a9853dda44866f3b
kvqn <guneetaggarwal@gmail.com>
2023-04-25 11:15:10 +0530

    influencer model:     added socials
    campaign models:
        lot of changes (see diff)

--- echiofy/campaign/models.py
+++ echiofy/campaign/models.py
@@ -1,6 +1,6 @@
+from itertools import chain
 from django.db import models
 from enum import Enum
-from user.models import UserProfile
 
 class PrimaryObjectiveEnum(str, Enum):
     CONTENT_CREATION = 'Content Creation'
@@ -18,23 +18,71 @@ class PaymentTypeEnum(str, Enum):
         return self.value
 
 
+class CampaignAdditionalManagers(models.Model):
+    campaign = models.ForeignKey('Campaign', on_delete=models.CASCADE)
+    manager = models.ForeignKey('BrandMember', on_delete=models.CASCADE)
+
+    objects = models.Manager()
+
+class CampaignInfluencers(models.Model):
+    campaign = models.ForeignKey('Campaign', on_delete=models.CASCADE)
+    user = models.ForeignKey('user.UserProfile', on_delete=models.CASCADE)
+
+    objects = models.Manager()
 
 class Campaign(models.Model):
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
     id = models.AutoField(primary_key=True)
-    owner_user = models.ForeignKey('user.UserProfile', on_delete=models.CASCADE, blank=False)
     name = models.CharField(max_length=100, blank=False)
     description = models.TextField()
     primary_objective = models.CharField(max_length=50, choices=[(tag.value, tag.value) for tag in PrimaryObjectiveEnum])
-    brand = models.CharField(max_length=100)
     payment_type = models.CharField(max_length=50, choices=[(tag.value, tag.value) for tag in PaymentTypeEnum])
     budget = models.IntegerField()
     payment_delay_days = models.IntegerField()
     tentative_payout = models.IntegerField()
-    influencers = models.ManyToManyField('user.Influencer', blank=True)
 
-    def __str__(self):
-        return self.name
+    @property
+    def managers(self):
+        return chain(self.brand.managers, CampaignAdditionalManagers.objects.filter(campaign=self)) #type: ignore
 
+    @property
+    def influencers(self):
+        return CampaignInfluencers.objects.filter(campaign=self)
+
+    objects = models.Manager()
+
+class BrandMember(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey('user.UserProfile', on_delete=models.CASCADE)
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+    is_manager = models.BooleanField(default=False) #type: ignore
+
+    @property
+    def campaigns(self):
+        if self.is_manager:
+            return self.brand.campaigns #type: ignore
+        return chain(self.brand.campaigns, CampaignAdditionalManagers.objects.filter(manager=self)) #type: ignore
+
+    objects = models.Manager()
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField()
+
+    @property
+    def members(self):
+        return BrandMember.objects.filter(brand=self)
+
+    @property
+    def managers(self):
+        return BrandMember.objects.filter(brand=self, is_manager=True)
+
+    @property
+    def campaigns(self):
+        return Campaign.objects.filter(brand=self)
+
+    objects = models.Manager()
 
 
 
