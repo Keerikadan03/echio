a574ba39f3893351525069e76b0549e66da429f7
kvqn <guneetaggarwal@gmail.com>
2023-04-27 21:23:06 +0530

    added some models to admin

--- echiofy/user/admin.py
+++ echiofy/user/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from .models import *
 
-admin.site.register(UserProfile, UserAdmin)
+admin.site.register(UserProfile)
 admin.site.register(Influencer)
